66c7ae5a6eab7e9306e8c24a74f277d16b4407b7
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-02 21:40:15 +0000

    updated syntax to print number useing .format()

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,5 +3,6 @@ import random
 number = random.randint(-10000, 10000)
 s = ['Last digit of', 'is', 'and is greater than 5', 'and is 0',
      'and is less than 6 and not 0']
-n = number % 10 if number >= 0 else number % 10 * -1
-print(s[0], number, s[1], n, s[3] if n == 0 else s[2] if n > 5 else s[4])
+n = number % 1
+print(s[0], "{:d}".format(number), s[1], "{:d}".format(n),
+      s[3] if n == 0 else s[2] if n > 5 else s[4])
