3b90746bfb062a78e7e58a32d4800660604d4fd2
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-02 21:33:30 +0000

    updated string syntax to single quotes

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-s = ["Last digit of", "is", "and is greater than 5", "and is 0",
-     "and is less than 6 and not 0"]
+s = ['Last digit of', 'is', 'and is greater than 5', 'and is 0',
+     'and is less than 6 and not 0']
 n = number % 10
 print(s[0], number, s[1], n, s[3] if n == 0 else s[2] if n > 5 else s[4])
