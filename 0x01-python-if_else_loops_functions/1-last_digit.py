549ea3be3fb61c75097e07707acd728f0671b107
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-02 21:36:55 +0000

    updated to print -1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,5 +3,5 @@ import random
 number = random.randint(-10000, 10000)
 s = ['Last digit of', 'is', 'and is greater than 5', 'and is 0',
      'and is less than 6 and not 0']
-n = number % 10
+n = number % 10 if number >= 0 else number % 10 * -1
 print(s[0], number, s[1], n, s[3] if n == 0 else s[2] if n > 5 else s[4])
