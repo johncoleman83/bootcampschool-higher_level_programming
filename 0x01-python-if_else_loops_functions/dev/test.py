097cced325341a4af3e5090a11da5cf7767666e0
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-03 03:04:05 +0000

    remove nth char from string

--- 0x01-python-if_else_loops_functions/dev/test.py
+++ 0x01-python-if_else_loops_functions/dev/test.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-for x in range(1,99):
-    if x % 3 == 0:
-        print("Fizz", end="")
-    if x % 5 == 0:
-        print("Buzz", end="")
-    if x % 3 != 00 and x % 5 != 0:
-        print(x, end="")
-    print(" ", end="")
+for x in range(1, 101):
+    if x % 3 != 0 and x % 5 != 0:
+        print(x, "", end="")
+    if x % 5 == 0 and x % 3 == 0:
+        print("FizzBuzz ", end="")
+    elif x % 3 == 0:
+        print("Fizz ", end="")
+    elif x % 5 == 0:
+        print("Buzz ", end="")
