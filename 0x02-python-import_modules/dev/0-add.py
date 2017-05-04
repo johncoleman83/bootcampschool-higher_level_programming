bc9875b52cc70efa1ca2fa30033172fa3d1c6de4
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-04 22:21:16 +0000

    print 5 calculations:

--- 0x02-python-import_modules/dev/0-add.py
+++ 0x02-python-import_modules/dev/0-add.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 from add_0 import add
-print('1 + 2 = {:d}'.format(add(1, 2)))
+
+
+def main():
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    main()
