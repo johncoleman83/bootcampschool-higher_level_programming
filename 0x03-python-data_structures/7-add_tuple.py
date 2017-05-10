1833c5c43d9fd6d3c9523f877fd47bdbe49a1a7e
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-10 04:47:45 +0000

    add 2 tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 
-def 
-add_tuple(tuple_a=(), tuple_b=()):
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = [0, 0]
+    for n in range(2):
+        try:
+            if tuple_a[n]:
+                a[n] += tuple_a[n]
+            if tuple_b[n]:
+                a[n] += tuple_b[n]
+        except:
+            pass
+    return a
--- 0x03-python-data_structures/dev/7-add_tuple.py
+++ 0x03-python-data_structures/dev/7-add_tuple.py
@@ -2,4 +2,13 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    
+    a = [0, 0]
+    for n in range(2):
+        try:
+            if tuple_a[n]:
+                a[n] += tuple_a[n]
+            if tuple_b[n]:
+                a[n] += tuple_b[n]
+        except:
+            pass
+    return a
