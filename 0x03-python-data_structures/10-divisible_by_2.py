f45abffe75b6fd6be04a9ea72b17867309910989
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-10 02:46:48 +0000

    updated for input of None

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+def 
+divisible_by_2(my_list=[]):
--- 0x03-python-data_structures/dev/5-main.py
+++ 0x03-python-data_structures/dev/5-main.py
@@ -2,5 +2,7 @@
 no_c = __import__('5-no_c').no_c
 
 print(no_c("Holberton School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
+print(no_c("C"))
+print(no_c("C is fun! don't you c"))
+print(no_c(""))
+print(no_c(None))
