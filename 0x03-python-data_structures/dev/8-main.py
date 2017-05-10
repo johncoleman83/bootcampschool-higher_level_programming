34cf048386aa0d2e75cd02f9700ace3d8fe20fbf
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-10 05:22:20 +0000

    tuple of 2

--- 0x03-python-data_structures/dev/8-main.py
+++ 0x03-python-data_structures/dev/8-main.py
@@ -2,5 +2,6 @@
 multiple_returns = __import__('8-multiple_returns').multiple_returns
 
 sentence = "At Holberton school, I learnt C!"
+sentence = ""
 length, first = multiple_returns(sentence)
 print("Length: {:d} - First character: {}".format(length, first))
--- 0x03-python-data_structures/dev/8-multiple_returns.py
+++ 0x03-python-data_structures/dev/8-multiple_returns.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
-
 def multiple_returns(sentence):
-    
+    l = len(sentence)
+    return (l, sentence[0] if l > 0 else None)
