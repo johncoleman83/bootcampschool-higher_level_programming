4cd401f524ad58a1b25238c7a75b25e708a9be79
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-10 05:06:13 +0000

    changed algorithm

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,12 +3,10 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     a = [0, 0]
-    for n in range(2):
-        try:
-            if tuple_a[n]:
-                a[n] += tuple_a[n]
-            if tuple_b[n]:
-                a[n] += tuple_b[n]
-        except:
-            pass
+    tup = (tuple_a, tuple_b)
+    for i in range(2):
+        if len(tup[0]) > i:
+            a[i] += tup[0][i]
+        if len(tup[1]) > i:
+            a[i] += tup[1][i]
     return (a[0], a[1])
