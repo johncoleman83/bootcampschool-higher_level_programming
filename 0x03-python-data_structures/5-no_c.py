45fed1ed245ce999b7d61a402e17e242be9d109f
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-10 04:10:18 +0000

    refactored 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,21 +3,6 @@
 
 def no_c(my_string):
     if my_string:
-        i = 0
-        while len(my_string) > 0:
-            l = len(my_string)
-            if my_string[i] == 'c' or my_string[i] == 'C':
-                if i == 0:
-                    if i < l - 1:
-                        my_string = my_string[1:]
-                    else:
-                        my_string = ""
-                elif i == l - 1:
-                    my_string = my_string[:l - 1]
-                    break
-                else:
-                    my_string = my_string[:i] + my_string[i + 1:]
-            i += 1
-            if i >= len(my_string):
-                break
-    return my_string
+        my_string = [l for l in list(my_string) if l.lower() != 'c']
+        my_string = ''.join(my_string)
+    return (my_string)
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,9 +6,13 @@ def print_matrix_integer(matrix=[[]]):
         i = 0
         while i < len(matrix):
             j = 0
-            l = len(matrix[i])
-            while j < l:
-                print("{:d}{:}".format(matrix[i][j],
-                                       (' ' if j < l - 1 else '\n')), end="")
-                j += 1
+            if matrix[i]:
+                l = len(matrix[i])
+                while j < l:
+                    print("{:d}{:}".format(matrix[i][j],
+                                           (' ' if j < l - 1
+                                            else '\n')), end="")
+                    j += 1
+            else:
+                print(matrix[i])
             i += 1
