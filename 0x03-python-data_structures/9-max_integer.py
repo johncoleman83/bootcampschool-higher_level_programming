5150fe5eea10ea90b8c478c4507dab00eab2a84b
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-10 05:25:45 +0000

    find max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-def 
-max_integer(my_list=[]):
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    m = my_list[0]
+    for i in my_list:
+        if i > m:
+            m = i
+    return m
