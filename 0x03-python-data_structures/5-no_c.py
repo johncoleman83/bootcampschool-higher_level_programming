1a73d20ab8335a272079cfd5206999401cb46bc9
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-10 04:13:02 +0000

    refactored

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@
 
 def no_c(my_string):
     if my_string:
-        my_string = [l for l in list(my_string) if l.lower() != 'c']
+        my_string = [l for l in my_string if l.lower() != 'c']
         my_string = ''.join(my_string)
     return (my_string)
