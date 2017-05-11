61f97264a47917cda47258bd4f90d3b8d56ac01f
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-11 22:02:43 +0000

    refactored

--- 0x04-python-more_data_structures/dev/10-best_score.py
+++ 0x04-python-more_data_structures/dev/10-best_score.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def best_score(my_dict):
     if my_dict:
-        return sorted(list([k, v] for k, v in my_dict.items()),
-                      key=lambda x: x[1])[-1][0]
+        return sorted(my_dict)[-1]
