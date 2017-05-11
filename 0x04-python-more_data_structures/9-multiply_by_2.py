ff4fb9d585808fcb71ada85621dacd1fb61135d7
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-11 20:45:35 +0000

    refactored 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(my_dict):
-    adict = {}
-    for key in my_dict:
-        adict[key] = my_dict[key] * 2
-    return adict
+    return {k: v*2 for k, v in my_dict.items()}
