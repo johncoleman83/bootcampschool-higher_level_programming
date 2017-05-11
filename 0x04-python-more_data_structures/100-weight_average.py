e8ba48263fe86a5b639f26dc7b0428994d4621ee
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-11 23:57:32 +0000

    updated for empty input

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    sum, items = 0, 0
-    for x, y in my_list:
-        sum += x * y
-        items += y
-    return sum / items
+    if my_list:
+        sum, items = 0, 0
+        for x, y in my_list:
+            sum += x * y
+            items += y
+        return sum / items
+    return 0
