64a92f4fedce7729625d8e7b8d9a87fd7bc897f9
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-11 19:08:00 +0000

    updated to len of row

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[row[i]**2 for i in range(len(matrix))] for row in matrix]
+    return [[row[i]**2 for i in range(len(row))] for row in matrix]
