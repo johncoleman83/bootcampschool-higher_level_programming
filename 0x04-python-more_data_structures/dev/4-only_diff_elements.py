528dc5e6f5370ef06ed569461d5b8ebd4c6a7abc
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-11 19:53:30 +0000

    len of dictionary

--- 0x04-python-more_data_structures/dev/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/dev/4-only_diff_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    return set([x for x in set_1 if x not in set_2]
-               + [y for y in set_2 if y not in set_1])
+    return set([x for x in set_1 if x not in set_2] +
+               [y for y in set_2 if y not in set_1])
