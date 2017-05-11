e8b69a40230b887288179cfc1d6c8bfe7e59f114
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-11 20:19:09 +0000

    updated format style

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(my_dict):
     for akey in sorted([key for key in my_dict]):
-        print("{:}: ".format(akey), my_dict[akey], sep='')
+        print("{}: {}".format(akey, my_dict[akey]))
