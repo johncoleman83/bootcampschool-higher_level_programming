542b88b58769cb9eba5a063ff7908632aeff7a36
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-12 21:49:47 +0000

    updating exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,6 @@ def safe_print_list_integers(my_list=[], x=0):
                 print("{:d}".format(my_list[i]), end='')
                 count += 1
         except (TypeError, ValueError):
+            pass
     print()
     return count
--- 0x05-python-exceptions/dev/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/dev/2-safe_print_list_integers.py
@@ -6,9 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
             if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end='')
                 count += 1
-        except (TypeError, ValueError) as e:
-            print(e)
-            print('testing here')
-            break
+        except (TypeError, ValueError):
+            pass
     print()
     return count
