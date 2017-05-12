cd8c5e0065e074e9a1edddd4b6d1238cc60733e7
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-12 21:04:59 +0000

    updated without len()

--- 0x05-python-exceptions/dev/0-main.py
+++ 0x05-python-exceptions/dev/0-main.py
@@ -5,6 +5,9 @@ my_list = [1, 2, 3, 4, 5]
 a = []
 b = ['hi', 'there', 'how', 'are', 'you']
 c = [my_list, a, b]
+d = [(5, 3, 6), 66, "string"]
+e = [(1, 2), (4, 5)]
+f = [5]
 nb_print = safe_print_list(my_list, 2)
 print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list(my_list, len(my_list))
@@ -16,3 +19,10 @@ print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list(b, 2)
 print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list(c, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(d, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(e, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(f, 2)
+print("nb_print: {:d}".format(nb_print))
--- 0x05-python-exceptions/dev/0-safe_print_list.py
+++ 0x05-python-exceptions/dev/0-safe_print_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    l = len(my_list)
-    for i in range(0, x if x < l else l):
+    for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end='')
             count += 1
