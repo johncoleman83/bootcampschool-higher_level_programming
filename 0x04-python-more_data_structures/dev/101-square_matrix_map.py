80823f9199fd00461a885ac1876d0b2bc1046b81
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-12 00:49:53 +0000

    refactored

--- 0x04-python-more_data_structures/dev/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/dev/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_map(m=[]):
-    return list(map(lambda i: list(map(lambda y: y**2, m[i])), range(len(m))))
+def square_matrix_map(matrix=[]):
+    return list(map(lambda row: list(map(lambda y: y**2, row)), matrix))
