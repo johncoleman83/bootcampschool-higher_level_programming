63fbf661366410f6ad7d9271fc48864ec72ea581
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-15 18:16:27 +0000

    updated tests and removed try/except from function

--- 0x06-python-test_driven_development/0-add_integer.py
+++ 0x06-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 def add_integer(a, b):
-    try:
-        if a == int(a) or a == float(a):
-            a = int(a)
-            try:
-                if b == int(b) or b == float(b):
-                    b = int(b)
-            except:
-                raise TypeError("b must be an integer")
-    except:
+    if isinstance(a, int) or isinstance(a, float):
+        if isinstance(b, int) or isinstance(b, float):
+            return int(a) + int(b)
+        else:
+            raise TypeError("b must be an integer")
+    else:
         raise TypeError("a must be an integer")
-    return a + b
