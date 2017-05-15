db411f424b8beef5eddeb7df843667af7fee4094
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-15 23:20:12 +0000

    printing a square

--- 0x06-python-test_driven_development/4-print_square.py
+++ 0x06-python-test_driven_development/4-print_square.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
 def print_square(size):
+    if isinstance(size, int):
+        if size == 0:
+            print()
+        elif size > 0:
+            for j in range(size):
+                for i in range(size):
+                    print('#', end='')
+                print('')
+        else:
+            raise ValueError('size must be >= 0')
+    else:
+        raise TypeError('size must be an integer')
