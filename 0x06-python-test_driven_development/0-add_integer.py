13d0195dc963897693a64c64964d9a7ab6768d26
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-15 17:24:59 +0000

    first testing cases added

--- 0x06-python-test_driven_development/0-add_integer.py
+++ 0x06-python-test_driven_development/0-add_integer.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 def add_integer(a, b):
+    try:
+        if a == int(a) or a == float(a):
+            a = int(a)
+            try:
+                if b == int(b) or b == float(b):
+                    b = int(b)
+            except:
+                raise TypeError("b must be an integer")
+    except:
+        raise TypeError("a must be an integer")
+    return a + b
