5977080c2dcc2d046aa051bf3ef95040fad276f7
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-15 22:28:20 +0000

    updated tests for say my name

--- 0x06-python-test_driven_development/3-say_my_name.py
+++ 0x06-python-test_driven_development/3-say_my_name.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def say_my_name(first_name, last_name=""):
+    if isinstance(first_name, str) and isinstance(last_name, str):
+        print('My name is {:} {:}'.format(first_name, last_name))
+    else:
+        raise TypeError('{:} must be a string'
+                        .format('last_name' if isinstance(first_name, str)
+                                else 'first_name'))
