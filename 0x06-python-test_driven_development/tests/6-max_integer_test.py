24f370016a2d43887738175fbd9718c334909192
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-16 23:33:13 +0000

    updated for more expected failures

--- 0x06-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x06-python-test_driven_development/tests/6-max_integer_test.py
@@ -35,9 +35,58 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1, 999999999999999]), 999999999999999)
 
 
+    def test_boolean(self):
+        self.assertEqual(max_integer([True, False, 1, 2, 3]), 3)
+
+
+    def test_negatives(self):
+        self.assertEqual(max_integer([-5, -1, -8]), -1)
+
+
+    def test_negatives(self):
+        self.assertIsNone(max_integer([]))
+
+
+    def test_none_in_list(self):
+        self.assertIsNone(max_integer([None]))
+
+
+class ExpectedFailureTestCase(unittest.TestCase):
+
+
     @unittest.expectedFailure
     def test_string_in_list(self):
         self.assertEqual(max_integer(["hi", 1, 2, 3]), 3)
+
+
+    @unittest.expectedFailure
+    def test_none(self):
+        self.assertEqual(max_integer(None), 4)
+
+
+    @unittest.expectedFailure
+    def test_none1(self):
+        self.assertEqual(max_integer([None, 5]), 5)
+
+
+    @unittest.expectedFailure
+    def test_none2(self):
+        self.assertEqual(max_integer([4, None, 5]), 5)
+
+
+    @unittest.expectedFailure
+    def test_none3(self):
+        self.assertEqual(max_integer([4, None]), 5)
+
+
+    @unittest.expectedFailure
+    def test_none4(self):
+        self.assertEqual(max_integer([None, None, None]), None)
+
+
+    @unittest.expectedFailure
+    def test_too_many_args(self):
+        self.assertEqual(max_integer([1, 2, 3], [4, 5]), 3)
 
 
     @unittest.expectedFailure
