eb9832ed5e399406db1bff5c9ce7a253e315c7e5
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-18 13:58:59 +0000

    updated for pep8

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,7 +6,7 @@ def main():
     a = 10
     b = 5
     sym = ['+', '-', '*', '/']
-    op = {0:add, 1:sub, 2:mul, 3:div}
+    op = {0: add, 1: sub, 2: mul, 3: div}
     for i in op:
         print('{:d} {:} {:d} = {:d}'.format(a, sym[i], b, op[i](a, b)))
 
