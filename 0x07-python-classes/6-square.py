33c8040cd114ac7050379c02b1979ac75865f570
johncoleman83 <lcsw@davidjohncoleman.com>
2017-05-23 16:07:07 +0000

    updated print to account for size of 0 for input size of square

--- 0x07-python-classes/6-square.py
+++ 0x07-python-classes/6-square.py
@@ -53,12 +53,13 @@ class Square:
         """ prints a square of '#' """
         if self.__size == 0:
             print()
-        if (self.__position):
-            for i in range(self.__position[1]):
-                print()
-            for i in range(self.__size):
-                print(' ' * self.__position[0], end='')
-                print('#' * self.__size)
         else:
-            for i in range(self.__size):
-                print('#' * self.__size)
+            if (self.__position):
+                for i in range(self.__position[1]):
+                    print()
+                for i in range(self.__size):
+                    print(' ' * self.__position[0], end='')
+                    print('#' * self.__size)
+            else:
+                for i in range(self.__size):
+                    print('#' * self.__size)
