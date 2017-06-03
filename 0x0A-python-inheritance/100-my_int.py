828a1bde23e6a8eb1ff0722460674b09df1a9f0e
johncoleman83 <lcsw@davidjohncoleman.com>
2017-06-03 06:34:40 +0000

    added blank line

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,5 +3,6 @@ class MyInt(int):
     """class to override int comparrisons"""
     def __eq__(self, other):
         return int(self) != int(other)
+
     def __ne__(self, other):
         return int(self) == int(other)
