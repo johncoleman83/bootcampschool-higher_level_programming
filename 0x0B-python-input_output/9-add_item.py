b484a328362e30c877910a5b984dc314568fec57
johncoleman83 <lcsw@davidjohncoleman.com>
2017-06-03 04:31:10 +0000

    updated to use functions

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -10,9 +10,7 @@ def app():
     a_list = []
     for i in range(1, len(argv)):
         a_list.append(argv[i])
-    with open('./add_item.json', mode='w', encoding='utf-8') as f_io:
-        f_io.write(json.dumps(a_list))
-        f_io.close()
+    save_to_json_file(a_list, './add_item.json')
 
 
 if __name__ == '__main__':
