aa5e1d99732829db0f4a2ce02144c19913ce7371
johncoleman83 <lcsw@davidjohncoleman.com>
2017-06-03 06:16:52 +0000

    updated parent class to not init instance

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -2,12 +2,8 @@
 """module evaluate geometry using maths operations"""
 
 
-class BaseGeometry:
+class BaseGeometry(object):
     """Geometry class with maths operations"""
-    def __init__(self):
-        """initializes new object of BaseGeometry Class"""
-        pass
-
     def area(self):
         """defines area of shape"""
         raise Exception("area() is not implemented")
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,23 +1,6 @@
 #!/usr/bin/python3
 """module evaluate geometry using maths operations"""
-
-
-class BaseGeometry:
-    """Geometry class with maths operations"""
-    def __init__(self):
-        """initializes new object of BaseGeometry Class"""
-        pass
-
-    def area(self):
-        """defines area of shape"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates if input:value is integer"""
-        if (value.__class__ != int):
-            raise TypeError("{:} must be an integer".format(name))
-        if (value <= 0):
-            raise ValueError("{:} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
