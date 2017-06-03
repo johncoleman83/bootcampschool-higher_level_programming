c20a2eb2de6c4b68665b69d887a79a2aef617813
johncoleman83 <lcsw@davidjohncoleman.com>
2017-06-03 03:24:13 +0000

    initializing directory and beginning read file scripts

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def number_of_lines(filename=""):
+    """returns number of lines in a file"""
+    with open(filename, mode='r', encoding='utf-8') as a_file:
+        for i, l in enumerate(a_file):
+            pass
+    return i + 1
