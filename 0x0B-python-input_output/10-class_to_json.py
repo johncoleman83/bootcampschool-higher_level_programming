55a4991e2e69b80ea1233a149226b0411228b210
johncoleman83 <lcsw@davidjohncoleman.com>
2017-06-03 17:17:26 +0000

    added function to return dict representation of class attributes with comments

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def class_to_json(obj):
+    """returns dict representation of class"""
     try:
         obj_dict = obj.__dict__
         return obj_dict
--- 0x0B-python-input_output/dev/11-student.py
+++ 0x0B-python-input_output/dev/11-student.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+class Student():
+    """Student class with name and age"""
+    def __init__(self, first_name, last_name, age):
+        """initializes new instance of Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+
+    def to_json(self):
+        """returns dict attributes of Student"""
+        try:
+            obj_dict = self.__dict__
+            return obj_dict
+        except:
+            return {}
