2c44bd25deff011662729bb82910cd3b7a3f7ed8
johncoleman83 <lcsw@davidjohncoleman.com>
2017-06-03 04:25:32 +0000

    write to .json file

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+import json
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+from sys import argv
+
+
+def app():
+    """adds input args to file"""
+    a_list = []
+    for i in range(1, len(argv)):
+        a_list.append(argv[i])
+    with open('./add_item.json', mode='w', encoding='utf-8') as f_io:
+        f_io.write(json.dumps(a_list))
+        f_io.close()
+
+
+if __name__ == '__main__':
+    app()
