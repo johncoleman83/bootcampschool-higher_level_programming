bde86aaf8596bc10e62c9e72262a3bceb4b699e7
johncoleman83 <lcsw@davidjohncoleman.com>
2017-06-03 03:28:48 +0000

    updated count lines of a file

--- 0x0B-python-input_output/1-main.py
+++ 0x0B-python-input_output/1-main.py
@@ -4,4 +4,6 @@ number_of_lines = __import__('1-number_of_lines').number_of_lines
 filename = "my_file_0.txt"
 nb_lines = number_of_lines(filename)
 print("{} has {:d} lines".format(filename, nb_lines))
-
+filename = "emptyfile.txt"
+nb_lines = number_of_lines(filename)
+print("{} has {:d} lines".format(filename, nb_lines))
--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def number_of_lines(filename=""):
     """returns number of lines in a file"""
+    lines = 0
     with open(filename, mode='r', encoding='utf-8') as a_file:
         for i, l in enumerate(a_file):
-            pass
-    return i + 1
+            lines += 1
+    return lines
