d25a1e41738ff83a68b52fc08889ab1bd78b0b40
johncoleman83 <lcsw@davidjohncoleman.com>
2017-06-03 07:01:00 +0000

    refactored 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import json
 from sys import argv
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
@@ -7,13 +6,10 @@ save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
 def app():
     """adds input args to file"""
-    a_list = []
     try:
-        b_list = load_from_json_file('./add_item.json')
-        for i in b_list:
-            a_list.append(i)
+        a_list = load_from_json_file('./add_item.json')
     except:
-        pass
+        a_list = []
     for i in range(1, len(argv)):
         a_list.append(argv[i])
     save_to_json_file(a_list, './add_item.json')
