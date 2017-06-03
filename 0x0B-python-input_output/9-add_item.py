defba2eb876d60d1d664627fffb65fdd3773b96e
johncoleman83 <lcsw@davidjohncoleman.com>
2017-06-03 04:37:10 +0000

    updated to append to file

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -7,7 +7,7 @@ save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
 def app():
     """adds input args to file"""
-    a_list = []
+    a_list = load_from_json_file('./add_item.json')
     for i in range(1, len(argv)):
         a_list.append(argv[i])
     save_to_json_file(a_list, './add_item.json')
