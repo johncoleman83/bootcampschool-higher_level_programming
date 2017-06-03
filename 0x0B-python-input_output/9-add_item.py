4c294fd2de3393d58befcc9827a4e69646a306e7
johncoleman83 <lcsw@davidjohncoleman.com>
2017-06-03 04:26:43 +0000

    updated for pep8 style

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import json
+from sys import argv
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-from sys import argv
 
 
 def app():
