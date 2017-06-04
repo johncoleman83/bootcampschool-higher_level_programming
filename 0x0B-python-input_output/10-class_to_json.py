7ed79cdf1452cf90bb0fd5574732ca05c57b444a
johncoleman83 <lcsw@davidjohncoleman.com>
2017-06-04 00:42:20 +0000

    added parenthesis

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -2,8 +2,7 @@
 def class_to_json(obj):
     """returns dict representation of class"""
     try:
-        o_D = obj.__dict__
-        D = dict([k, v] for k, v in o_D.items())
-        return D
+        o_D = (obj.__dict__)
+        return o_D
     except:
         return {}
