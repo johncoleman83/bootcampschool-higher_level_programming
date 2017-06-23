dc8ab94343b9dde371dd69396d3895a407aa1261
johncoleman83 <lcsw@davidjohncoleman.com>
2017-06-23 16:08:45 -0700

    changed instance attributes to private

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """ Instantiation of rectangle with width and height """
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
         Rectangle.number_of_instances += 1
 
     @property
