e8920a975ffbde510872ec9964184b1a4fc9620c
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-12 16:46:00 -0700

    MySQL scripts to view table data from input database

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,1 +1,3 @@
--- lists all rows of the table first_table from the database hbtn_0c_0 in MySQL server
+-- lists all rows of the table first_table from the database hbtn_0c_0 of
+-- MySQL server
+SELECT * FROM first_table;
