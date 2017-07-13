e73bb2c735b422d803217e8390ab5f0babda5aa4
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-12 17:05:11 -0700

    show table records and insert new rows

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,1 +1,3 @@
--- inserts a new row in the table first_table (database hbtn_0c_0) in MySQL server
+-- inserts a new row in the table first_table (database hbtn_0c_0) in
+-- MySQL server
+INSERT INTO first_table (id, name) VALUES(98, 'Holberton School');
