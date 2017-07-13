759b5fc96b022751ffbcba7fa8f6b7af2158ca45
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-12 23:59:24 -0700

    list records of mySQL server

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,1 +1,4 @@
--- lists all records of the table second_table of the database hbtn_0c_0 in MySQL server
+-- lists all records of the table second_table of the database hbtn_0c_0 in
+-- MySQL server
+SELECT score, name FROM second_table
+WHERE name IS NOT NULL ORDER BY score DESC;
