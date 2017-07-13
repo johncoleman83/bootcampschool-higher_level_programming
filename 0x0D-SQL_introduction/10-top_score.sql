0630f9bfd4d1e9d43028e8595c0e5896e1bb7ae1
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-12 17:35:10 -0700

    select certain cols and order them

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,1 +1,3 @@
--- lists all records of the table second_table of the database hbtn_0c_0 in MySQL server
+-- lists all records of the table second_table of the database hbtn_0c_0
+-- in MySQL server
+SELECT score, name FROM second_table ORDER BY score DESC;
