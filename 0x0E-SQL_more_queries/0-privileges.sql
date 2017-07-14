a21add9fd4ce2ae06cec76649ad47ed6d17081ab
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-14 15:55:11 -0700

    updating scripts to create users, etc...

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,1 +1,4 @@
--- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on MySQL server
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2
+-- from MySQL server
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
