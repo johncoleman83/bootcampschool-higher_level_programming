fdcbd7951f9f31ee8ab6afeef6ec2f671f1ec243
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-14 21:44:45 -0700

    updated scripts to build primary and foreeign keys

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
 -- creates the table unique_id on your MySQL server
 CREATE TABLE IF NOT EXISTS unique_id
-(id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+	   (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
