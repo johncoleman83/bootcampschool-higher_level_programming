541a5a48ff2cb2292c9d61f39f9d8c97cb7a61ef
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-16 08:24:25 -0700

    changed heading of genre name to "genre"

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,7 @@
 -- lists all genres from hbtn_0d_tvshows and displays the number of
 -- shows linked to each
-SELECT tv_genres.name, COUNT(tv_show_genres.genre_id) AS number_shows
+SELECT tv_genres.name AS genre,
+	   COUNT(tv_show_genres.genre_id) AS number_shows
        FROM tv_genres INNER JOIN tv_show_genres
        ON tv_show_genres.genre_id = tv_genres.id
        GROUP BY tv_genres.name
