1fb77d1cdec96f72d925fa44895db85fc68f7368
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-15 18:33:27 -0700

    list cities and states JOIN BY cities.state_id = states.id

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,4 +3,5 @@
 SELECT * FROM cities
        WHERE state_id =
        (SELECT id FROM states
-       WHERE name = 'California');
+       WHERE name = 'California')
+	   ORDER BY cities.id;
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,1 +1,5 @@
 -- lists all cities contained in the database hbtn_0d_usa
+SELECT id.cities, name.cities, name.states
+	   FROM cities INNER JOIN states
+       ON states.id = cities.state_id
+	   ORDER BY cities.id;
