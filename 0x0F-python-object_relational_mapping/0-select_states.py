3c301270fe07b9d12ac77841dd848fd630dba91c
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-08-10 14:11:31 -0700

    added port #

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,6 +6,7 @@ import MySQLdb
 import sys
 
 HOST = 'localhost'
+PORT = 3306
 UN = sys.argv[1]
 PW = sys.argv[2]
 DBNM = sys.argv[3]
@@ -15,7 +16,11 @@ def print_states():
     """
     function prints all states from input DB
     """
-    db = MySQLdb.connect(host=HOST, user=UN, passwd=PW, db=DBNM)
+    db = MySQLdb.connect(host=HOST,
+                         port=PORT,
+                         user=UN,
+                         passwd=PW,
+                         db=DBNM)
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = cur.fetchall()
