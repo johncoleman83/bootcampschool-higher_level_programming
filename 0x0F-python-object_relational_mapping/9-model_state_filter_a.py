1e13d5f8321b10368966512754b1c5bc608c65e8
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-08-10 20:52:50 -0700

    sqlalchemy printing all states with "a" in the name

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -2,3 +2,27 @@
 """
 9-model_state_filter_a.py
 """
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import sessionmaker
+
+
+def init_sess():
+    """initializes session instance of DB using sqlalchemy"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
+
+
+def print_a_states(session):
+    """prints all states with 'a' in the name"""
+    instances = session.query(State).filter(State.name.like('%a%'))
+    for instance in instances:
+        print(instance.id, ': ', instance.name, sep='')
+
+
+if __name__ == '__main__':
+    print_a_states(init_sess())
