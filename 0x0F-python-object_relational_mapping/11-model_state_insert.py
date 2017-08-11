3dc5de38c344f6aba94ce23bc204e31a73606802
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-08-11 14:19:46 -0700

    adding new state to DB with sqlalchemy

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -2,3 +2,38 @@
 """
 11-model_state_insert.py
 """
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import sessionmaker
+
+
+def init_sess():
+    """initializes session instance of DB using sqlalchemy"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
+
+
+def parse_input(s):
+    """parses input removing single quotes and semicolons"""
+    REP_CHARS = [';', "'", '"']
+    for i in REP_CHARS:
+        s = s.replace(i, '--')
+    return s
+
+
+def add_state(session):
+    """adds state to the DB"""
+    s = State()
+    s.name = "Louisiana"
+    session.add(s)
+    session.commit()
+    r = session.query(State).filter(State.name == "Louisiana")
+    print(r[0].id)
+
+
+if __name__ == '__main__':
+    add_state(init_sess())
