47c0ad703a6525eeb52c2a3789b4234e2f099e3c
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-08-11 15:56:13 -0700

    City Class for sqlalchemy and query for all cities

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+14-model_city_fetch_by_state.py
+"""
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def init_sess():
+    """initializes session and engine for sqlalchemy DB instance"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return (engine, session)
+
+
+def print_states(db):
+    """prints all the cities from session DB"""
+    session = db[1]
+    i = session.query(City, State).filter(State.id == City.state_id).\
+        order_by(City.id)
+    for c, s in i:
+        print(s.name, ': ({}) '.format(c.id), c.name, sep='')
+    session.close()
+    db[0].dispose()
+
+if __name__ == '__main__':
+    print_states(init_sess())
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,3 +2,14 @@
 """
 model_city.py
 """
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base
+
+
+class City(Base):
+    """defines cities table properties"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
