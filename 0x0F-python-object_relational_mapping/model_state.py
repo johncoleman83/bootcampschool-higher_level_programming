96e1ad7ab4c04f27db0512fb429c9c18ba8300bb
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-08-10 18:14:50 -0700

    first use of sqlalchemy; created base model with states table definitions

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,3 +2,14 @@
 """
 model_state.py
 """
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """defines states table properties"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
