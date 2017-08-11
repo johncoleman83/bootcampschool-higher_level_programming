d1063f5d98785f612bc645d4391027232d094189
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-08-10 19:03:26 -0700

    using sqlalchemy to query database for states & min state ID, then printing results

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,3 +2,25 @@
 """
 7-model_state_fetch_all.py
 """
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def init_sess():
+    """initializes session and engine for sqlalchemy DB instance"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
+
+
+def print_states(session):
+    """prints all the states from session DB"""
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, ': ', instance.name, sep='')
+
+if __name__ == '__main__':
+    print_states(init_sess())
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -2,3 +2,29 @@
 """
 8-model_state_fetch_first.py
 """
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import sessionmaker
+
+
+def init_sess():
+    """initializes session instance of DB using sqlalchemy"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
+
+
+def print_min_state(session):
+    """prints the state with min value using first()"""
+    instance = session.query(State).first()
+    try:
+        print(instance.id, ': ', instance.name, sep='')
+    except:
+        print("Nothing")
+
+
+if __name__ == '__main__':
+    print_min_state(init_sess())
