0e47013967426d2172f1daad728cab0045b686b7
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-08-28 13:25:57 -0700

    request url with name values parameters

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,3 +2,24 @@
 """
 2-post_email.py
 """
+import urllib.request
+import urllib.parse
+import sys
+
+
+def value_request_id(the_url, the_email):
+    """makes a request to input URL with email as a parameter"""
+    values = {'email': the_email}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(the_url, data)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+    print("{}".format(html.decode('utf8')))
+
+
+if __name__ == "__main__":
+    """MAIN APP"""
+    the_url = sys.argv[1]
+    the_email = sys.argv[2]
+    value_request_id(the_url, the_email)
