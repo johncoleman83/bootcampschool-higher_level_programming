e48c51dc94ca2db87435149be5036662dbc1263a
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-08-28 16:07:12 -0700

    make a request to https://swapi.co/documentation star wars API for people search from input string as search parameter

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -2,3 +2,22 @@
 """
 9-starwars.py
 """
+import requests
+import sys
+
+
+def request_to_star_wars(the_url):
+    """makes a request to input URL with q as a parameter"""
+    r = requests.get(the_url)
+    json = r.json()
+    count = json.get('count')
+    print("Number of result: {}".format(count))
+    if count > 0:
+        results = json.get('results')
+        for character in results:
+            print(character.get('name'))
+
+if __name__ == "__main__":
+    """MAIN APP"""
+    the_url = "https://swapi.co/api/people/?search={}".format(sys.argv[1])
+    request_to_star_wars(the_url)
