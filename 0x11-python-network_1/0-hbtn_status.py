ed1f768cc5c7cc92d673f0859af18cfe0ca80503
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-08-28 12:38:57 -0700

    makes request and displays content

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,3 +2,19 @@
 """
 0-hbtn_status.py
 """
+import urllib.request
+
+
+def display_request_data(the_url):
+    """makes a request to specified URL & displays data on the request"""
+    with urllib.request.urlopen(the_url) as response:
+        html = response.read()
+    print("Body response:")
+    print("    - type: {}".format(type(html)))
+    print("    - content: {}".format(html))
+    print("    - utf8 content: {}".format(html.decode('utf8')))
+
+
+if __name__ == "__main__":
+    """MAIN APP"""
+    display_request_data("https://intranet.hbtn.io/status")
