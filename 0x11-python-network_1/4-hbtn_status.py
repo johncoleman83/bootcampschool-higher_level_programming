5f607c782f3e64c6bdf8b9d3352b4c9e446d8b7d
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-08-28 14:08:57 -0700

    make request and print request data using python requests lib

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,3 +2,19 @@
 """
 4-hbtn_status.py
 """
+import requests
+
+
+def display_request_data(the_url):
+    """makes a request to specified URL & displays data on the request"""
+    r = requests.get(the_url)
+    html = r.content
+    print("Body response:")
+    print("    - type: {}".format(type(html)))
+    print("    - content: {}".format(html))
+    print("    - utf8 content: {}".format(html.decode('utf8')))
+
+
+if __name__ == "__main__":
+    """MAIN APP"""
+    display_request_data("https://intranet.hbtn.io/status")
