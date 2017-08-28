c6fab6d59e76d41deb37782ade01878b81ded9ab
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-08-28 14:51:02 -0700

    make request and check for error response codes

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,3 +2,19 @@
 """
 7-error_code.py
 """
+import requests
+import sys
+
+
+def request_error_check(the_url):
+    """makes a request to input URL with checks for errors"""
+    r = requests.get(the_url)
+    if r.status_code > 200:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print("{}".format(r.text))
+
+if __name__ == "__main__":
+    """MAIN APP"""
+    the_url = sys.argv[1]
+    request_error_check(the_url)
