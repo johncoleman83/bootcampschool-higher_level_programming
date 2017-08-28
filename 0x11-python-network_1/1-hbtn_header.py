a043b223f2863e9d14b329a6e76c05ddf7a8ab05
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-08-28 13:05:21 -0700

    print X-Request-Id header value from input URL

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,3 +2,18 @@
 """
 1-hbtn_header.py
 """
+import urllib.request
+import sys
+
+
+def value_request_id(the_url):
+    """makes a request to input URL & displays value of X-Request-Id"""
+    with urllib.request.urlopen(the_url) as response:
+        headers = response.info()
+    print("{}".format(headers.get("X-Request-Id")))
+
+
+if __name__ == "__main__":
+    """MAIN APP"""
+    the_url = sys.argv[1]
+    value_request_id(the_url)
