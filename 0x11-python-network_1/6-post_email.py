df842a9b815918dc3ff35fc7d4da72b50bb91b26
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-08-28 14:43:42 -0700

    make request to input url with requests lib with email parameter from input

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,3 +2,18 @@
 """
 6-post_email.py
 """
+import requests
+import sys
+
+
+def request_with_parameter(the_url, the_email):
+    """makes a request to input URL with email as a parameter"""
+    payload = {'email': the_email}
+    r = requests.post(the_url, data=payload)
+    print("{}".format(r.text))
+
+if __name__ == "__main__":
+    """MAIN APP"""
+    the_url = sys.argv[1]
+    the_email = sys.argv[2]
+    request_with_parameter(the_url, the_email)
