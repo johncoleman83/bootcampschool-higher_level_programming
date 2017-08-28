2c2226e66317c03a101d6f94a2eea6b1a4d444ae
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-08-28 16:36:52 -0700

    make request to github api

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,3 +2,18 @@
 """
 10-my_github.py
 """
+import requests
+from requests.auth import HTTPBasicAuth
+import sys
+
+
+def request_to_github(the_url, un, pw):
+    """makes a request to input URL with q as a parameter"""
+    r = requests.get(the_url, auth=HTTPBasicAuth(un, pw))
+    json = r.json()
+    print("{}".format(json.get("id")))
+
+if __name__ == "__main__":
+    """MAIN APP"""
+    the_url = 'https://api.github.com/user'
+    request_to_github(the_url, sys.argv[1], sys.argv[2])
