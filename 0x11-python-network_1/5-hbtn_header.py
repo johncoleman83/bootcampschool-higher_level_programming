8b85c5ba74ad91f262db52217e7d814a8bba078a
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-08-28 14:17:02 -0700

    get X-Request-Id from input URL using requests library in python

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,3 +2,17 @@
 """
 5-hbtn_header.py
 """
+import requests
+import sys
+
+
+def value_request_id(the_url):
+    """makes a request to input URL & displays value of X-Request-Id"""
+    r = requests.get(the_url)
+    print("{}".format(r.headers.get("X-Request-Id")))
+
+
+if __name__ == "__main__":
+    """MAIN APP"""
+    the_url = sys.argv[1]
+    value_request_id(the_url)
