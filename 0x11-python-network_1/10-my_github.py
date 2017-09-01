a1cd67c4fa4f1138d1ad3e8757876ce13a61c1b6
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-01 10:35:19 -0700

    updated variable names

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -10,8 +10,8 @@ import sys
 def request_to_github(the_url, un, pw):
     """makes a request to input URL with q as a parameter"""
     r = requests.get(the_url, auth=HTTPBasicAuth(un, pw))
-    json = r.json()
-    print("{}".format(json.get("id")))
+    the_json = r.json()
+    print("{}".format(the_json.get("id")))
 
 if __name__ == "__main__":
     """MAIN APP"""
