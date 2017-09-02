b140e19b6ded6735757869d6df5c5ed313d080c3
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-02 10:09:04 -0700

    requests latest 10 commits from github API

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,3 +2,22 @@
 """
 100-github_commits.py
 """
+import requests
+import sys
+
+
+def request_to_github(the_url):
+    """makes a request for commits from input url"""
+    r = requests.get(the_url)
+    the_json = r.json()
+    for i in range(10):
+        c = the_json[i]
+        c_id = c.get('sha')
+        c_author = c['commit']['author']['name']
+        print('{}: {}'.format(c_id, c_author))
+
+if __name__ == "__main__":
+    """MAIN APP"""
+    the_url = ('https://api.github.com/repos/{}/{}/commits'
+               .format(sys.argv[2], sys.argv[1]))
+    request_to_github(the_url)
