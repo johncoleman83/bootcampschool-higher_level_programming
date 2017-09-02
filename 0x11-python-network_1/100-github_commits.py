d57d42c9d63a0ac0f353fc913a6b69b211afdf03
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-02 10:42:16 -0700

    removed order by author date and used default commit date

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,19 +10,12 @@ def request_to_github(the_url):
     """makes a request for commits from input url"""
     r = requests.get(the_url)
     the_json = r.json()
-    results = []
     c_range = 10 if len(the_json) >= 10 else len(the_json)
     for i in range(c_range):
         c = the_json[i]
         c_sha = c.get('sha')
         c_author = c.get('commit').get('author').get('name')
-        # print('{}: {}'.format(c_sha, c_author))
-        c_date = c.get('commit').get('author').get('date')
-        results.append([c_date, c_sha, c_author])
-    results = list(reversed(sorted(results)))
-    for i in range(10):
-        c = results[i]
-        print('{}: {}'.format(c[1], c[2]))
+        print('{}: {}'.format(c_sha, c_author))
 
 if __name__ == "__main__":
     """MAIN APP"""
