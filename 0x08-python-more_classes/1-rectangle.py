bb74c60a94be5fb74c989636cb3e854674e13c69
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-04 16:58:30 -0700

    updateding init function to properly use the getter and setter

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """ This is a Rectangle Class (it defines a rectangle) """
     def __init__(self, width=0, height=0):
         """ Instantiation of rectangle with width and height """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """ Instantiation of rectangle with width and height """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
