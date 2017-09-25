ff5bd4223af8359232ace728a8f7f4bac337c3bf
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-25 16:23:16 -0700

    reverses a list and returns same list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
 /* 8-esrever.js */
-exports.esrever = function (list)
+exports.esrever = function (list) {
+  let end = list.length - 1;
+  for (let i = 0; i < list.length / 2; i++, end--) {
+    [list[i], list[end]] = [list[end], list[i]];
+  }
+  return list;
+};
