cd1537347e427c2e3ad5a30406eee4e0db7ebc92
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-26 13:01:17 -0700

    readfile asynchronously from process.argv input file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,2 +1,12 @@
 #!/usr/bin/node
 /* 0-readme.js */
+
+var fs = require('fs');
+var file = process.argv[2];
+fs.readFile(file, 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.slice(0, data.length - 1));
+  }
+});
