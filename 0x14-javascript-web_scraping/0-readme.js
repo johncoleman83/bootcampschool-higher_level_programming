989fef1e6d8269effb3230b675007bf8300ce47a
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-26 13:30:39 -0700

    removed if conditional to check for argv length, removed modification of data, and added the writeFile functionality

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,16 +2,12 @@
 /* 0-readme.js */
 
 const fs = require('fs');
-let file;
-if (process.argv.length > 2) {
-  file = process.argv[2];
-} else {
-  file = '';
-}
+let file = process.argv[2];
+
 fs.readFile(file, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
-    console.log(data.slice(0, data.length - 1));
+    console.log(data);
   }
 });
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,2 +1,18 @@
 #!/usr/bin/node
 /* 1-writeme.js */
+
+const fs = require('fs');
+let file;
+let data;
+if (process.argv.length > 3) {
+  file = process.argv[2];
+  data = process.argv[3];
+} else {
+  file = '';
+  data = '';
+}
+fs.writeFile(file, data, 'utf8', function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
