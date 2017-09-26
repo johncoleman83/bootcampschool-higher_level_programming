6f3e13dfdc6170fb258760c996c6aa40a558d224
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-25 19:07:01 -0700

    keeps track of number of function calls

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
 /* 9-logme.js */
-exports.logMe = function (item)
+exports.logMe = function (item) {
+  if (this.count === undefined) {
+    this.count = 0;
+  }
+  console.log(this.count + ':', item);
+  this.count++;
+};
