97ce4dbae225392fda493936675255faa385382c
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-26 16:18:09 -0700

    updated requests methods to utilize another method asynchronously

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,28 +2,32 @@
 /* 4-starwars_count.js */
 
 const request = require('request');
-const wedgeAntilles = '18';
-let count = 0;
-let url = process.argv[2];
+const url = process.argv[2];
 request.get(url, function (error, response, body) {
   if (error) {
-    console.log(error);
+    console.log(0);
   }
   if (response && body) {
-    if (response.headers['content-type'] === 'application/json') {
-      body = JSON.parse(body);
-      let results = body.results;
-      for (let result in results) {
-        let characters = results[result].characters;
-        if (characters) {
-          for (let person in characters) {
-            if (characters[person].includes(wedgeAntilles)) {
-              count++;
-            }
+    findWedgeAntilles(response, body);
+  }
+});
+
+function findWedgeAntilles (response, body) {
+  const wedgeAntilles = '18';
+  let count = 0;
+  if (response.headers['content-type'] === 'application/json') {
+    body = JSON.parse(body);
+    let results = body.results;
+    for (let result in results) {
+      let characters = results[result].characters;
+      if (characters) {
+        for (let person in characters) {
+          if (characters[person].includes(wedgeAntilles)) {
+            count++;
           }
         }
       }
     }
   }
   console.log(count);
-});
+}
--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,2 +1,23 @@
 #!/usr/bin/node
 /* 5-request_store.js */
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const file = process.argv[3];
+request.get(url, function (error, response, body) {
+  if (error) {
+    console.log(0);
+  }
+  if (response && body) {
+    writeBody(body);
+  }
+});
+
+function writeBody (body) {
+  fs.writeFile(file, body, 'utf8', function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+}
