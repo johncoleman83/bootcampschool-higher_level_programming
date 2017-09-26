fde2255013b6a0b8834a49db5ab21a3bae02b168
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-25 19:27:05 -0700

    function that returns function to convert to the given base, similar to NumberObject.toString(radix)

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
 /* 10-converter.js */
-exports.converter = function (base)
+exports.converter = function (base) {
+  return function (num) {
+    return num.toString(base);
+  };
+};
