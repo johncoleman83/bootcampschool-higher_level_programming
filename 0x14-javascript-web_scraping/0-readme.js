10ea89df7d1f446dd9ce4d34596d3cb53d11172a
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-26 13:11:43 -0700

    updated var variables to const and let

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
 /* 0-readme.js */
 
-var fs = require('fs');
-var file = process.argv[2];
+const fs = require('fs');
+let file;
+if (process.argv.length > 2) {
+  file = process.argv[2];
+} else {
+  file = '';
+}
 fs.readFile(file, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
