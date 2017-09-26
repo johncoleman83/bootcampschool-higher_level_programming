56121a317aa305d16e0df8136477ef17974f6666
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-26 14:16:52 -0700

    makes request and returns the status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,2 +1,14 @@
 #!/usr/bin/node
 /* 2-statuscode.js */
+
+const request = require('request');
+let url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  if (response) {
+    console.log(response.statusCode);
+  }
+});
