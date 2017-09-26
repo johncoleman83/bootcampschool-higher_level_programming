f372df396c92e0b65a07286052b88072e49c39e7
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-26 15:30:11 -0700

    API request to SWAPI that searches for Wedge Achilles movies

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,2 +1,23 @@
 #!/usr/bin/node
 /* 4-starwars_count.js */
+
+const request = require('request');
+const wedgeAntilles = 'https://swapi.co/api/people/18/';
+let count = 0;
+let url = process.argv[2];
+request.get(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  if (body) {
+    body = JSON.parse(body);
+    let films = body.results;
+    for (let film = 0; film < films.length; film++) {
+      let characters = films[film].characters;
+      if (characters.indexOf(wedgeAntilles) > -1) {
+        count++;
+      }
+    }
+    console.log(count);
+  }
+});
