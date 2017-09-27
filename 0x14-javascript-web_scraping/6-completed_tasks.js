617e0c38f364f4e54e723530abe2d73c6b189a67
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-26 21:30:46 -0700

    makes request to test API and returns tasks per user

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,2 +1,27 @@
 #!/usr/bin/node
 /* 6-completed_tasks.js */
+
+const request = require('request');
+const url = process.argv[2];
+request.get(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    computeTasks(body);
+  }
+});
+
+function computeTasks (body) {
+  const usersObj = Object();
+  for (let i = 1; i < 11; i++) {
+    usersObj[i.toString(10)] = 0;
+  }
+  const tasksByUser = JSON.parse(body);
+  for (let userTask in tasksByUser) {
+    let userId = tasksByUser[userTask].userId.toString();
+    if (tasksByUser[userTask].completed) {
+      usersObj[userId]++;
+    }
+  }
+  console.log(usersObj);
+}
