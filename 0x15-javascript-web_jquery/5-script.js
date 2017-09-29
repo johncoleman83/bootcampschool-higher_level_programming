5659800b85ed1e3ff81cd4083a8f3978d97f4fbb
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-28 22:05:23 -0700

    replace element

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,1 +1,6 @@
 /* 5-script.js */
+let add_item = $('DIV#add_item');
+add_item.on('click', function () {
+  let my_list = $('UL.my_list');
+  my_list.append('<li>Item</li>');
+});
--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,1 +1,6 @@
 /* 6-script.js */
+let updateHeader = $('DIV#update_header');
+updateHeader.on('click', function () {
+  let header = $('HEADER');
+  header.replaceWith('<header>New Header!!!</header>');
+});
