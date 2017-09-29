edd593f8225db6d3b81eab7056cefe2cb69a467e
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-28 21:30:46 -0700

    jQuery modify element based on current class

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,1 +1,12 @@
 /* 4-script.js */
+let toggle = $('DIV#toggle_header');
+toggle.on('click', function () {
+  let header = $('header');
+  if (header.hasClass('red')) {
+    header.removeClass('red');
+    header.addClass('green');
+  } else {
+    header.removeClass('green');
+    header.addClass('red');
+  }
+});
