49ab44ee61882838454274b65ce3cd38fcede2c6
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-09-29 23:14:04 -0700

    testing api with swapi

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,1 +1,5 @@
 /* 7-script.js */
+const $ = window.$;
+$.get('https://swapi.co/api/people/5/?format=json', function (data) {
+  $('DIV#character').text(data.name);
+});
--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,9 @@
 /* 8-script.js */
+const $ = window.$;
+$.get('https://swapi.co/api/films/?format=json', function (data) {
+  let results = data.results;
+  for (let i in results) {
+    $('UL#list_movies').append('<LI>' + results[i].title + '</LI>');
+  }
+  // $('UL#list_movies').text(data.name);
+});
